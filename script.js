f1f139fcdb337a170130a46f78bcb284d530a6e6
Matheus Boechat <matheusfgb@gmail.com>
2023-02-07 23:54:44 -0300

    requisito 12

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 const inputTaskText = document.getElementById('texto-tarefa');
 const taskButton = document.getElementById('criar-tarefa');
-const taskListItens = document.getElementsByClassName('task-list-item');
+const listItem = 'task-list-item';
+const taskListItens = document.getElementsByClassName(listItem);
 const taskOlList = document.getElementById('lista-tarefas');
 const deleteButton = document.getElementById('apaga-tudo');
 const concludedDeleteButton = document.getElementById('remover-finalizados');
+const localStorageButton = document.getElementById('salvar-tarefas');
+
+const getAllClasses = (objectItem) => {
+  let classes = '';
+  const classesLength = Object.keys(objectItem.classesFromItem).length;
+  for (let index = 0; index < classesLength; index += 1) {
+    if (objectItem.classesFromItem[index + 1] === undefined) {
+      classes += `${objectItem.classesFromItem[index]}`;
+    } else {
+      classes += `${objectItem.classesFromItem[index]} `;
+    }
+  }
+  return classes;
+};
+
+const detectLocalStorage = () => {
+  if (localStorage.getItem('savedItens') !== null) {
+    return true;
+  }
+  return false;
+};
+
+const objectAttribute = () => {
+  if (detectLocalStorage()) {
+    return JSON.parse(localStorage.getItem('savedItens'));
+  }
+  return {};
+};
+
+const stockList = objectAttribute();
+if (detectLocalStorage()) {
+  for (let index = 0; index < Object.keys(stockList).length; index += 1) {
+    const li = document.createElement('li');
+    li.innerHTML = stockList[index].itemText;
+    li.className = getAllClasses(stockList[index]);
+    taskOlList.appendChild(li);
+  }
+}
 
 taskButton.addEventListener('click', () => {
   const li = document.createElement('li');
-  li.className = 'task-list-item';
+  li.className = listItem;
   li.innerHTML = inputTaskText.value;
   taskOlList.appendChild(li);
   inputTaskText.value = '';
@@ -15,18 +54,18 @@ taskButton.addEventListener('click', () => {
 
 taskOlList.addEventListener('click', (event) => {
   const wholeList = event.target;
-  if (wholeList.classList.contains('task-list-item')) {
-    const greyBg = document.querySelector('#gray-bg');
+  if (wholeList.classList.contains(listItem)) {
+    const greyBg = document.querySelector('.gray-bg');
     if (greyBg !== null) {
-      greyBg.removeAttribute('id');
+      greyBg.classList.remove('gray-bg');
     }
-    wholeList.id = 'gray-bg';
+    wholeList.classList.add('gray-bg');
   }
 });
 
 taskOlList.addEventListener('dblclick', (event) => {
   const wholeList = event.target;
-  if (wholeList.classList.contains('task-list-item')) {
+  if (wholeList.classList.contains(listItem)) {
     if (wholeList.classList.contains('completed')) {
       wholeList.classList.remove('completed');
     } else {
@@ -46,4 +85,13 @@ concludedDeleteButton.addEventListener('click', () => {
   for (let index = completed.length - 1; index >= 0; index -= 1) {
     completed[index].remove();
   }
+});
+
+localStorageButton.addEventListener('click', () => {
+  for (let index = 0; index < taskListItens.length; index += 1) {
+    const itemText = taskListItens[index].innerText;
+    const classesFromItem = taskListItens[index].classList;
+    stockList[index] = { itemText, classesFromItem };
+  }
+  localStorage.setItem('savedItens', JSON.stringify(stockList));
 });
