c3b6cafecddffe77600f63b507085f9cd864863a
Matheus Boechat <matheusfgb@gmail.com>
2023-02-07 21:31:11 -0300

    requisitos 6 a 8

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const inputTaskText = document.getElementById('texto-tarefa');
 const buttonTask = document.getElementById('criar-tarefa');
 const taskOlList = document.getElementById('lista-tarefas');
 
-const newTaskListItem = () => {
+buttonTask.addEventListener('click', () => {
   const li = document.createElement('li');
   li.className = 'task-list-item';
   li.innerHTML = inputTaskText.value;
   taskOlList.appendChild(li);
   inputTaskText.value = '';
-};
+});
 
-buttonTask.addEventListener('click', newTaskListItem);
+taskOlList.addEventListener('click', (event) => {
+  const wholeList = event.target;
+  if (wholeList.classList.contains('task-list-item')) {
+    const greyBg = document.querySelector('#gray-bg');
+    if (greyBg !== null) {
+      greyBg.removeAttribute('id');
+    }
+    wholeList.id = 'gray-bg';
+  }
+});
